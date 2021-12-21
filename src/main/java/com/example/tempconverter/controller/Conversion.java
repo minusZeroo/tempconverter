76066fa1ab9319ca9d1509f2126f89f9bdf6ef68
minusZeroo <pcijideani@gmail.com>
2021-12-21 11:17:15 +0200

    refactor

--- src/main/java/com/example/tempconverter/controller/Conversion.java
+++ src/main/java/com/example/tempconverter/controller/Conversion.java
@@ -17,13 +17,13 @@ public class Conversion {
             return  (data_t + 273.15f);
         }else if(type.equals("fahrenheit") && fromUnit.equals("celsius")) {
             return  ((data_t*9)/5) + 32;
-        }else if(type.equals("celsius") && fromUnit.equals("fahrenheit")){
+        }else if(type.equals("celsius") && fromUnit.equals("fahrenheit")) {
             return ((data_t-32)*5) / 9;
-        }else  if(type.equals("kelvin") && fromUnit.equals("fahrenheit")){
+        }else  if(type.equals("kelvin") && fromUnit.equals("fahrenheit")) {
             return  273.5f + ((data_t - 32.0f) * (5.0f/9.0f));
-        }else if(type.equals("celsius") && fromUnit.equals("kelvin")){
+        }else if(type.equals("celsius") && fromUnit.equals("kelvin")) {
             return  ( data_t + 273.15f );
-        }else if(type.equals("fahrenheit") && fromUnit.equals("kelvin")){
+        }else if(type.equals("fahrenheit") && fromUnit.equals("kelvin")) {
             return ((data_t - 273.15f) * 1.8f) + 32;
         }
         return data_t;
